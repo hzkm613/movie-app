8e0a35fdababbd0eefbfe25ba0ff52478e01fe7f
hzkm613 <hzkm613@gmail.com>
2025-01-23 20:37:34 -0500

    Display movies

--- src/App.js
+++ src/App.js
@@ -23,7 +23,22 @@ console.log(movies);
 
 return(
   <div>
-    {loading ? <h1>Loading...</h1> : null}
+    {loading ? (
+      <h1>Loading...</h1>
+     ) : (
+     <div>
+      {movies.map(movie => 
+        (<div key={movie.id}>
+          <img src={movie.medium_cover_image} />
+          <h2>{movie.title}</h2>
+          <p>{movie.summary}</p>
+          <ul>
+            {movie.genres.map(g => <li key={g}>{g}</li>)}
+          </ul>
+        </div>
+      ))}
+    </div>
+    )}
   </div>
   );
 }
